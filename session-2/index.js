6d7bd4c431184ccc621613d60b719a41b4e8f9fd
sari <you@example.com>
2022-01-19 19:10:43 +0000

    cahnge of variables

--- session-2/index.js
+++ session-2/index.js
@@ -1,2 +1,8 @@
 // This is a comment in JavaScript
 // You can see where this file is connected to your HTML in the index.html on line 14
+const Greeting  = "Hello World"
+console.log(Greeting);
+
+let Greeting1  = "Hello World 2"
+
+console.log(Greeting1);
--- session-3/index.js
+++ session-3/index.js
@@ -1,2 +1,11 @@
 // This is a comment in JavaScript
 // You can see where this file is connected to your HTML in the index.html on line 14
+const thisIsNumber = 5; //
+
+// this is not the same variable
+const THIS_IS_A_NUMBER = 5;
+console.log(thisIsNumber)
+
+let name = 'Sarika';
+var age = 10;
+console.log(name)
