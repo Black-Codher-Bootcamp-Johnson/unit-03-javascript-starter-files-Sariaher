4658e703eb9dda8829021c252ff2df4519dba5e2
sari <you@example.com>
2022-01-24 20:07:18 +0000

    maths operators in JS

--- session-4/index.js
+++ session-4/index.js
@@ -1,8 +1,35 @@
 // ****************************************************TASK 1 - OBJECTS****************************************************
+function biography(person)  {
+    return 'Hi, my name is ' + person.name + '. I am '   +  person.age +  ' years old, I live in ' + person.location +  'and I like ' + person.likes +  '.' 
+    }                                
+     
+let  personA={
+    name: 'A',
+    age:'21',
+    location:'Manchester',
+    likes: 'pizza',
 
+};
+console.log(biography(personA));
 
+                                                                                                                                                                                         
+ let  personB={
+    name: 'B',
+    age:'31',
+    location:'Manchester',
+    likes: 'pasta',
+ };
 
+    console.log(biography(personB));
 
+let  personC= {
+    name: 'C',
+    age:'11',
+    location:'London',
+    likes: 'K BBQ',
+    
+};
+console.log(biography(personC))
 
 
 
@@ -19,27 +46,35 @@
 // ****************************************************TASK 2 - MATHS OPERATORS****************************************************
 // ***Uncomment the code below to begin the task***
 
-// const x = 6;
-// const y = 4;
+const x = 6;
+const y = 4;
 
-// const addition = x + y;
+const addition = x + y;
 
-// console.log("Addition: x + y " + addition);
+console.log("Addition: x + y = " + addition);
 
-// const subtraction = x - y;
+const subtraction = x - y;
 
-// console.log("Subtraction: x - y " + subtraction);
+console.log("Subtraction: x - y = " + subtraction);
 
-// const multiplication = x * y;
+const multiplication = x * y;
 
-// console.log("multiplication: x * y " + multiplication);
+console.log("multiplication: x * y = " + multiplication);
 
-// const division = x * y;
+const division = x * y;
 
-// console.log("Division: x + y " + division);
+console.log("Division: x + y = " + division);
 
+const multiplication1= x * x;
 
+console.log("multiplication1: x * x = " + multiplication1);
 
+const addition1 = x + x + y;
+
+console.log("Addition1: x + x + y = " + addition1);
+
+const muldiv =  x * x /y;
+ console.log("muldiv: x*x/y = " + muldiv);
 
 
 
