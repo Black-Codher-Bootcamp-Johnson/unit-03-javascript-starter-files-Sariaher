829ca5fce5a076526955fde08adf1ccb03b348cf
sari <you@example.com>
2022-01-26 20:12:48 +0000

    switchs

---
+++ session-5/index.js
@@ -0,0 +1,67 @@
+function getGrade(total) {
+  let grade;
+  switch (true) {
+    case total > 90:
+      grade = "A+";
+      break;
+    case total > 80:
+      grade = "A";
+      break;
+    case total > 70:
+      grade = "B";
+      break;
+    case total > 60:
+      grade = "C";
+      break;
+    case total > 50:
+      grade = "D";
+      break;
+    case total > 40:
+      grade = "E";
+      break;
+    case total > 30:
+      grade = "F";
+      break;
+    default:
+      grade = "Fail"
+  }
+      return grade;
+
+  
+}
+console.log(getGrade(92));
+
+
+
+
+
+
+//   function GetGrade(total) {
+//   if (total > 90) {
+//     return "A+";
+//   } else if (total > 80 && total <= 90) {
+//     return "A";
+//   } else if (total > 70 && total <= 80) {
+//     return "B";
+//   } else if (total > 60 && total <= 80) {
+//     return "C";
+//   } else if (total > 50 && total <= 60) {
+//     return "D";
+//   } else if (total > 40 && total <= 50) {
+//     return "E";
+//   }  else if (total > 30 && total <= 40) {
+//     return "E"; 
+//   } else { return "Fail"}
+
+// }  
+
+// console.log(getGrade(92));
+
+// A+ if total is greater than 90.
+// A if total is greater than 80 and less than or equal to 90.
+// B if total is greater than 70 and less than or equal to 80.
+// C if total is greater than 60 and less than or equal to 70.
+// D if total is greater than 50 and less than or equal to 60.
+// E if total is greater than 40 and less than or equal to 50.
+//  F if total is greater than 30 and less than or equal to 40.
+// “Fail” is total is less than 30 /
