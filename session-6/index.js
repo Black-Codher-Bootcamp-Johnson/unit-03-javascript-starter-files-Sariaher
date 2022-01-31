75da2e0bebc2a1e6d7b743bfdd9e83e98941dd13
sari <you@example.com>
2022-01-31 18:53:10 +0000

    whileloop

--- session-6/index.js
+++ session-6/index.js
@@ -4,8 +4,11 @@ const randomNumber = Math.round(Math.random() * 10);
 // ****************************************************TASK 1****************************************************
 
 
-
-
+let counter = 1;
+while (counter <=10) {
+    console.log (randomNumber)
+    counter=counter+1
+}
 
 
 
